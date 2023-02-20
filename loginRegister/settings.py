4eff4e2120d59a28912dcc182412dbfef2984a35
suraj1kc <suraj1kc@gmail.com>
2023-02-20 14:46:46 +0545

    updated

--- loginRegister/settings.py
+++ loginRegister/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l)(mhdjr0zc+mc0ng1#p8#ue%son8zq_!w#m97o=u=06gfv!h1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
